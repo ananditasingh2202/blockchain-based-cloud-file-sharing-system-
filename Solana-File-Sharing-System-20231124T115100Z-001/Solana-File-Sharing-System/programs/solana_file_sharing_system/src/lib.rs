519b9d602aee21fa9fb02dc27d79c69d5f569c06
ananditasingh2202 <147225160+ananditasingh2202@users.noreply.github.com>
2024-04-22 11:50:45 +0530

    Add files via upload

---
+++ Solana-File-Sharing-System-20231124T115100Z-001/Solana-File-Sharing-System/programs/solana_file_sharing_system/src/lib.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+
+declare_id!("2sVP5kvLJbSvbNk6abeQjq4VDvd1p3AtMbJvuXVWNsR4");
+
+#[program]
+pub mod solana_file_sharing_system {
+    use super::*;
+    pub fn share_documents(ctx:Context<Leaks>,title:String,description:String,docs:String) -> Result<()> {
+        let files : &mut Account<Files> =  &mut ctx.accounts.files;
+        files.title = title;
+        files.description = description;
+        files.docs = docs;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Leaks<'info> {
+    #[account(init,payer=user,space=64*3)]
+    pub files: Account<'info,Files>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info,System>
+}
+
+#[account]
+pub struct Files{
+    pub title : String, 
+    pub description : String,
+    pub docs : String,
+}
